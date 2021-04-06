7ed924497f1d4c80ad2f93b254d3dd38330d702f
yahe0005 <yahe0005@stud.hs-kl.de>
2021-04-06 20:22:52 +0200

    flutter map eingebaut

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,12 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong/latlong.dart';
 
 class MapScreen extends StatelessWidget {
-  static const routeName = '/maps';
-
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Street Map Screen'),
-    );
+    return new FlutterMap(
+      options: new MapOptions(
+        center: new LatLng(51.5,-0.09),
+        zoom: 13.0,
+      ),
+      layers: [
+        new TileLayerOptions(
+          urlTemplate: "https://tile.openstreetmap.de/{z}/{x}/{y}.png",
+          additionalOptions: {
+            'id': 'mapbox.streets',
+          },
+        ),
+        new MarkerLayerOptions(
+          markers: [
+            new Marker(
+              width: 80.0,
+              height: 80.0,
+              point: new LatLng(51.5, -0.09),
+              builder: (ctx) =>
+              new Container(
+                child: new FlutterLogo(),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );;
   }
 }
+
+
