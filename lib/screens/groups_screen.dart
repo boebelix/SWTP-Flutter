fbca0b4a55258115e9f1d4593ef7a53cd09d7022
boebelix <sboebel@t-online.de>
2021-04-06 11:24:47 +0200

    Implementierung der Bottom Navigation Bar Umstellung auf NamedRoutes

---
+++ lib/screens/groups_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GroupsScreen extends StatelessWidget {
+  static const routeName = '/groups';
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Gruppe Screen'),
+    );
+  }
+}
