2a1dfa65b96d2a333264dcc0acb023aec5d25505
Saarli <mdmaurice.didion@gmail.com>
2021-04-09 16:41:46 +0200

    Mit screen begonnen, formatiert

--- lib/services/group_service.dart
+++ lib/services/group_service.dart
@@ -7,6 +7,7 @@ import 'package:swtp_app/models/member.dart';
 import 'package:swtp_app/models/user_memberships.dart';
 import 'package:swtp_app/services/auth_service.dart';
 
+
 class GroupService {
   static final GroupService _instance = GroupService._internal();
 
@@ -25,7 +26,8 @@ class GroupService {
 
   void loadGroupMemberships() async {
     _memberships.clear();
-    String response = await _userEndpoint.getMemberships(authService.user.userId);
+    String response =
+        await _userEndpoint.getMemberships(authService.user.userId);
 
     for (dynamic e in jsonDecode(response)) {
       _memberships.add(UserMemberships.fromJSON(e));
@@ -33,31 +35,41 @@ class GroupService {
   }
 
   void loadGroups() async {
-
     _groups.clear();
-    if (_memberships.isEmpty)
-      loadGroupMemberships();
+    if (_memberships.isEmpty) loadGroupMemberships();
 
-    Map<String, dynamic> response = await _groupsEndpoint.getGroupById(authService.user.userId);
-    _ownGroup=readGroupfromJson(response);
-
+    Map<String, dynamic> response =
+        await _groupsEndpoint.getGroupById(authService.user.userId);
+    _ownGroup = readGroupfromJson(response);
 
     for (UserMemberships membership in _memberships) {
-      _groups.add(readGroupfromJson(await _groupsEndpoint.getGroupById(membership.groupId)));
+      _groups.add(readGroupfromJson(
+          await _groupsEndpoint.getGroupById(membership.groupId)));
     }
   }
-}
 
-Group readGroupfromJson(Map<String, dynamic> json)
-{
-  Group gr= Group.fromJSON(json);
+  Group readGroupfromJson(Map<String, dynamic> json) {
+    Group gr = Group.fromJSON(json);
 
-  List<Member> groupmembers=List<Member>();
-  for(dynamic e in json['memberships'])
-  {
-    groupmembers.add(Member.fromJSON(e));
+    List<Member> groupmembers = List<Member>();
+    for (dynamic e in json['memberships']) {
+      groupmembers.add(Member.fromJSON(e));
+    }
+    gr.members = groupmembers;
+
+    return gr;
   }
-  gr.members=groupmembers;
 
-  return gr;
+  void denyInvitationOrLeaveGroup(int groupId) {
+    _groupsEndpoint.removeUserFromGroup(groupId, authService.user.userId);
+    loadGroups();
+  }
+
+  void kickUserFromOwnGroup(int userId) {
+    _groupsEndpoint.removeUserFromGroup(_ownGroup.groupId, userId);
+  }
+
+  void inviteUserToGroup(int userId) {
+    _groupsEndpoint.inviteUserToGroup(_ownGroup.groupId, userId);
+  }
 }
