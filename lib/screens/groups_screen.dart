d142470ffde5138ed44ab06b527aeac455be67b1
Saarli <mdmaurice.didion@gmail.com>
2021-04-10 15:33:57 +0200

    Screen für Gruppen angelegt. keine Funktionalität für Löschen

--- lib/screens/groups_screen.dart
+++ lib/screens/groups_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:swtp_app/generated/l10n.dart';
+import 'package:swtp_app/models/group.dart';
 import 'package:swtp_app/services/group_service.dart';
 
 class GroupsScreen extends StatefulWidget {
@@ -14,18 +15,54 @@ class _GroupsScreenState extends State<GroupsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+    return ListView(
+        shrinkWrap: true,
+        scrollDirection: Axis.vertical,
         children: <Widget>[
-          SingleChildScrollView(
+          Text(Language.of(context).groups,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              )),
+          ListView.builder(
+              padding: EdgeInsets.all(5),
+              shrinkWrap: true,
               scrollDirection: Axis.vertical,
-              child: Column(children: [
-                Text(Language.of(context).groups,
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.bold,
-                    ))
-              ]))
+              itemBuilder: (context, index) => createGroupCard(
+                  _groupService.acceptedGroups.elementAt(index))),
+          Text(Language.of(context).invitations,
+              style: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+              )),
+          ListView.builder(
+              padding: EdgeInsets.all(5),
+              shrinkWrap: true,
+              scrollDirection: Axis.vertical,
+              itemBuilder: (context, index) => createGroupCard(
+                  _groupService.invitetIntoGroups.elementAt(index))),
         ]);
+  }
+
+  Card createGroupCard(Group group) {
+    return Card(
+        elevation: 8,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: Column(
+          children: [
+            Text(group.groupName,
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                )),
+            Text("${group.admin.firstName} ${group.admin.firstName}",
+                style: TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.normal,
+                ))
+          ],
+        ));
   }
 }
