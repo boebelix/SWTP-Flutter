92c2b99266840f859f84bbec5e4d2f2ad4e8087d
yahe0005 <yahe0005@stud.hs-kl.de>
2021-04-10 16:26:13 +0200

    fix imports

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -4,8 +4,6 @@ import 'package:swtp_app/generated/l10n.dart';
 import 'package:swtp_app/models/login_credentials.dart';
 import 'package:swtp_app/providers/auth_endpoint_provider.dart';
 import 'package:swtp_app/screens/tabs_screen.dart';
-import 'package:swtp_app/services/information_pre_loader_service.dart';
-import 'package:swtp_app/services/auth_service.dart';
 import 'package:swtp_app/widgets/auth_endpoint_visualisation.dart';
 import 'package:swtp_app/widgets/register.dart';
 
