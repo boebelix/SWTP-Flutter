d2057487a97dfb3488aeddaa6b701f4afbc3da23
boebelix <sboebel@t-online.de>
2021-04-16 21:31:37 +0200

    Erste Versuche ein dynamische Radio Button zu erzeugen. Eine Lösung funktioniert auch soweit. Könnte auf List.View builder umgebaut werden.

---
+++ lib/widgets/add_poi_form.dart
@@ -0,0 +1,234 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:swtp_app/generated/l10n.dart';
+import 'package:swtp_app/models/category.dart';
+import 'package:swtp_app/models/notifier_state.dart';
+import 'package:swtp_app/widgets/warning_dialog.dart';
+import 'loading_indicator.dart';
+import 'package:swtp_app/providers/categories_service_provider.dart';
+
+class AddPoiForm extends StatefulWidget {
+  static const routeName = '/addPoiForm';
+
+  @override
+  _AddPoiFormState createState() => _AddPoiFormState();
+}
+
+class _AddPoiFormState extends State<AddPoiForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final TextEditingController _titleController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _categoryController = TextEditingController();
+
+  int selectedRadio;
+  int selectedRadioTile;
+  Category selectedCategory;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedRadio = 0;
+    selectedRadioTile = 0;
+  }
+
+  void setSelectedRadio(int val) {
+    setState(() {
+      selectedRadio = val;
+    });
+  }
+
+  void setSelectedRadioTile(int val) {
+    setState(() {
+      selectedRadioTile = val;
+    });
+  }
+
+  void setSelectedCategory(Category category) {
+    setState(() {
+      selectedCategory = category;
+    });
+  }
+
+  List<Widget> createCategoriesList(List<Category> categories) {
+    List<Widget> widgets = [];
+    for (Category category in categories) {
+      widgets.add(
+        RadioListTile(
+          value: category,
+          groupValue: selectedCategory,
+          title: Text(category.name),
+          subtitle: Text(category.categoryId.toString()),
+          onChanged: (currentCategory) {
+            print("Current Category ${currentCategory.name}");
+            setSelectedCategory(currentCategory);
+          },
+          selected: selectedCategory == category,
+        ),
+      );
+    }
+    return widgets;
+  }
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is removed from the widget tree.
+    // This also removes the _printLatestValue listener.
+    _titleController.dispose();
+    _descriptionController.dispose();
+    _categoryController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(Language.of(context).addNewPoi),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: Card(
+            elevation: 20,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: Form(
+              key: _formKey,
+              child: ListView(
+                padding: const EdgeInsets.all(8.0),
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: Language.of(context).newPoiTitle,
+                      icon: Icon(Icons.account_circle),
+                    ),
+                    validator: (value) {},
+                    controller: _titleController,
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: Language.of(context).newPoiDescription,
+                      icon: Icon(Icons.text_snippet),
+                    ),
+                    keyboardType: TextInputType.multiline,
+                    maxLines: null,
+                    validator: (value) {},
+                    controller: _descriptionController,
+                  ),
+                  Consumer<CategoriesServiceProvider>(
+                    builder: (_, notifier, __) {
+                      switch (notifier.state) {
+                        case NotifierState.initial:
+                          {
+                            return Container();
+                          }
+                          break;
+
+                        case NotifierState.loading:
+                          {
+                            return LoadingIndicator();
+                          }
+                          break;
+
+                        default:
+                          {
+                            return notifier.categoriesResponse.fold(
+                              //Fehlerfall
+                              (failure) {
+                                WidgetsBinding.instance.addPostFrameCallback((_) {
+                                  notifier.resetState();
+                                });
+                                return PopUpWarningDialog(
+                                  context: context,
+                                  failure: failure,
+                                );
+                              },
+                              // Alles in Ordnung
+                              (categories) {
+                                return Column(
+                                  children: [
+                                    Container(
+                                      padding: EdgeInsets.all(20.0),
+                                      child: Text("Dynamic Categories"),
+                                    ),
+                                    Column(
+                                      children: createCategoriesList(categories),
+                                    ),
+                                    RadioListTile(
+                                      value: 1,
+                                      groupValue: selectedRadioTile,
+                                      title: Text("Radio 1"),
+                                      subtitle: Text("Radio 1 Subtitile"),
+                                      onChanged: (value) {
+                                        print("Radio Tile pressed ${value}");
+                                        setSelectedRadioTile(value);
+                                      },
+                                      activeColor: Colors.red,
+                                      secondary: OutlineButton(
+                                        child: Text("Say hi"),
+                                        onPressed: () {
+                                          print("Say hello");
+                                        },
+                                      ),
+                                      selected: true,
+                                    ),
+                                    RadioListTile(
+                                      value: 2,
+                                      groupValue: selectedRadioTile,
+                                      title: Text("Radio 2"),
+                                      subtitle: Text("Radio 2 Subtitile"),
+                                      onChanged: (value) {
+                                        print("Radio Tile pressed ${value}");
+                                        setSelectedRadioTile(value);
+                                      },
+                                      activeColor: Colors.red,
+                                      secondary: OutlineButton(
+                                        child: Text("Say hi"),
+                                        onPressed: () {
+                                          print("Say hello");
+                                        },
+                                      ),
+                                      selected: false,
+                                    ),
+                                    Divider(
+                                      height: 20,
+                                      color: Colors.green,
+                                    ),
+                                    ButtonBar(
+                                      alignment: MainAxisAlignment.center,
+                                      children: [
+                                        Radio(
+                                          value: 1,
+                                          groupValue: selectedRadio,
+                                          activeColor: Colors.orange,
+                                          onChanged: (value) {
+                                            print(value);
+                                            setSelectedRadio(value);
+                                          },
+                                        ),
+                                        Radio(
+                                          value: 2,
+                                          groupValue: selectedRadio,
+                                          activeColor: Colors.orange,
+                                          onChanged: (value) {
+                                            print(value);
+                                            setSelectedRadio(value);
+                                          },
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                      }
+                    },
+                  ),
+                ],
+              ),
+            )),
+      ),
+    );
+  }
+}
