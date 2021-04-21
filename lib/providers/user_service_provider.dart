b869bc89f0eca69c6e87d5cb14af503dd927a3e3
Saarli <mdmaurice.didion@gmail.com>
2021-04-21 15:57:40 +0200

    Eigener User aus Liste der einladbaren Nutzer entfent

--- lib/providers/user_service_provider.dart
+++ lib/providers/user_service_provider.dart
@@ -6,6 +6,7 @@ import 'package:swtp_app/models/group.dart';
 import 'package:swtp_app/models/group_membership.dart';
 import 'package:swtp_app/models/notifier_state.dart';
 import 'package:swtp_app/models/user.dart';
+import 'package:swtp_app/services/auth_service.dart';
 
 class UserServiceProvider extends ChangeNotifier {
   static final UserServiceProvider _instance = UserServiceProvider._internal();
@@ -80,6 +81,7 @@ class UserServiceProvider extends ChangeNotifier {
       } else if (chosen && !usersToInvite.contains(usersNotInOwnGroup.elementAt(index))) {
         usersToInvite.add(usersNotInOwnGroup.elementAt(index));
       }
+      usersToInvite.remove(AuthService().user);
     }
   }
 
